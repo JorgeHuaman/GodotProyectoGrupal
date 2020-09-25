05535695972f459573a0b832e02ad9b4b8612f9f
JorgeHuaman <48896609+JorgeHuaman@users.noreply.github.com>
2020-09-25 09:56:18 -0500

    He logrado que el pueda saludar

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,5 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Hola mundo")
+	print("Ahora el personaje puede saludar")
